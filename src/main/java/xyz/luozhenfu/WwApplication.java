41a92402561d658702a7bd47cf12682ab5d60888
Administrator <742573123@qq.com>
2020-11-24 21:39:31 +0800

    456

--- src/main/java/xyz/luozhenfu/WwApplication.java
+++ src/main/java/xyz/luozhenfu/WwApplication.java
@@ -2,8 +2,10 @@ package xyz.luozhenfu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class WwApplication {
 
     public static void main(String[] args) {
